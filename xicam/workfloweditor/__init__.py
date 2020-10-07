a02ae0b2d29a5b6971b00ff59f55dae2c81acea9
ronpandolfi <ronpandolfi@gmail.com>
2020-10-07 16:19:34 -0700

    Refactor WorkflowEditor to use intents/canvases system

--- xicam/workfloweditor/__init__.py
+++ xicam/workfloweditor/__init__.py
@@ -1,14 +1,18 @@
 import numpy as np
+from databroker.in_memory import BlueskyInMemoryCatalog
 
+from xicam.core.execution.workflow import ingest_result_set
+from xicam.core.intents import PlotIntent
 from xicam.gui.widgets.imageviewmixins import BetterButtons, LogScaleIntensity
 
 from xicam.core import msg
 from xicam.core.execution import Workflow
 from xicam.plugins import GUIPlugin, GUILayout
 from xicam.gui.widgets.linearworkfloweditor import WorkflowEditor
-
-class ResultsView( BetterButtons, LogScaleIntensity):
-    pass
+# TODO: temporary code -- this should live in the views module (after view/model updated with layoutChanged)
+from qtpy.QtWidgets import QWidget, QVBoxLayout
+from xicam.SAXS.widgets.views import DataSelectorView, StackedCanvasView
+from xicam.XPCS.models import IntentsModel, EnsembleModel, Ensemble
 
 
 class WorkflowEditorPlugin(GUIPlugin):
@@ -19,14 +23,46 @@ class WorkflowEditorPlugin(GUIPlugin):
         self.workflow = Workflow()
         self.workfloweditor = WorkflowEditor(self.workflow, callback_slot=self.showResult, except_slot=lambda *_: self.workflow._pretty_print())
 
-        self.outputview = ResultsView(log_scale=False)
+        self.ensemble_model = EnsembleModel()
+
+        data_selector_view = DataSelectorView()
+        data_selector_view.setModel(self.ensemble_model)
+
+        # Our results views' model (should only contain intents)
+        intents_model = IntentsModel()
+        intents_model.setSourceModel(self.ensemble_model)
+
+        # Our results view widget container
+        results_view = StackedCanvasView()
+        results_view.setModel(intents_model)
+
 
         self.stages = {
-            'Testing': GUILayout(self.outputview, left=None, lefttop=None, right=self.workfloweditor),
+            'Testing': GUILayout(results_view, left=None, righttop=data_selector_view, right=self.workfloweditor),
 
         }
         super(WorkflowEditorPlugin, self).__init__()
 
-    def showResult(self, result):
+    def showResult(self, *result):
         print('result:', result)
-        self.outputview.setImage(next(iter(result.values())), axes={"y":0, "x":1}, autoRange=False)
+
+        ensemble = Ensemble()
+        doc_generator = ingest_result_set(self.workflow, result)
+
+        #### TODO: replace with Dan's push-based
+        documents = list(doc_generator)
+        catalog = BlueskyInMemoryCatalog()
+        # TODO -- change upsert signature to put start and stop as kwargs
+        # TODO -- ask about more convenient way to get a BlueskyRun from a document generator
+        catalog.upsert(documents[0][1], documents[-1][1], ingest_result_set, [self.workflow, result], {})
+        catalog = catalog[-1]
+        ####
+
+        def project_intents(run_catalog):
+            return [PlotIntent(x=np.array([1,2,3]),
+                               y=np.array([1,2,3]),
+                               item_name='test',
+                               labels={'bottom': 'x', 'left': 'y'})]
+
+        ensemble.append_catalog(catalog)
+        self.ensemble_model.add_ensemble(ensemble, project_intents)
